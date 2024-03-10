95fe7253bfcf503381c7831f28f03334256bbdad
RigoMan1 <rodrigoalonso3781@gmail.com>
2024-03-10 03:01:12 -0500

    feat: implement basic card selection layout

--- app.vue
+++ app.vue
@@ -1,5 +1,93 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import TarotCards from '~/constants/tarot-card-data';
+
+type Spreads =
+  | 'three-card-cluster'
+  | 'pentagram-tarot'
+  | 'celtic-cross'
+  | 'seven-card-horseshoe';
+
+const randomCards = ref(TarotCards);
+
+const shuffleCards = () => {
+  let shuffled = [...TarotCards]; // Create a shallow copy to avoid modifying the original array
+  for (let i = shuffled.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1)); // Random index from 0 to i
+    [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]]; // Swap elements
+  }
+  randomCards.value = shuffled;
+};
+</script>
 
 <template>
-	<div class="container"></div>
+  <div class="container mt-20 arcana-scrollbar">
+    <div class="flex mt-8 overflow-auto p-4 max-w-7xl mx-auto">
+      <div
+        v-for="(card, cardIndex) in randomCards"
+        class="border border-zinc-500 relative p-4 first:rounded-l-lg last:rounded-r-lg"
+      >
+        <!-- counter badge on center top of card -->
+        <span
+          class="flex items-center justify-center absolute w-6 h-6 text-center -top-3 left-1/2
+            transform -translate-x-1/2 text-xs bg-zinc-900 border border-zinc-600
+            text-zinc-400 font-bold rounded-full"
+        >
+          {{ cardIndex + 1 }}
+        </span>
+        <tarot-card
+          :key="`${card.name}-${card.arcana}`"
+          :card="card"
+        />
+      </div>
+    </div>
+
+    <!-- Spreads Section -->
+    <div class="flex space-x-8 mt-8 max-w-[940px] mx-auto">
+      <div
+        v-for="card in ['past', 'present', 'future']"
+        class="border border-zinc-500 relative p-4 flex-1 h-[400px] rounded-lg"
+      >
+        <!-- counter badge on center top of card -->
+        <span
+          class="flex items-center justify-center absolute px-2 h-6 text-center -top-3 left-1/2
+            transform -translate-x-1/2 capitalize bg-zinc-900 border border-zinc-600
+            text-zinc-400 font-medium rounded-lg"
+        >
+          {{ card }}
+        </span>
+      </div>
+    </div>
+  </div>
 </template>
+
+<style>
+:root {
+  --color-primary: theme('colors.violet.500');
+  --color-primary-dark: #1e1e24;
+  --color-secondary: #cba135;
+}
+
+/* Custom Scrollbar Styles for the Tarot Reading App */
+.arcana-scrollbar ::-webkit-scrollbar {
+  width: 12px; /* Adjust width of scrollbar */
+}
+
+.arcana-scrollbar ::-webkit-scrollbar-track {
+  background: #1e1e24;
+  border-left: 2px solid var(--color-primary-dark); /* Darken the track to match the thumb */
+}
+
+.arcana-scrollbar ::-webkit-scrollbar-thumb {
+  background: var(--color-primary); /* Gold thumb */
+  border-radius: 6px; /* Rounded corners for the thumb */
+  border: 2px solid var(--color-primary-dark); /* Dark border to match the track */
+}
+
+.arcana-scrollbar ::-webkit-scrollbar-thumb:hover {
+  background: #ffd700; /* Lighter gold when hovered for an interactive feel */
+}
+
+.arcana-scrollbar ::-webkit-scrollbar-thumb:active {
+  background: #cba135; /* Return to original gold to signify active scrolling */
+}
+</style>
---
+++ components/TarotCard.vue
@@ -0,0 +1,145 @@
+<script setup lang="ts">
+type Arcana = 'major' | 'minor';
+type Suit = 'cup' | 'pentacle' | 'sword' | 'wand';
+type Court = 'knight' | 'page' | 'queen' | 'king';
+
+interface TarotCard {
+  name: string;
+  arcana: Arcana;
+  number: number | null;
+  suit: Suit | null;
+  court: Court | null;
+}
+
+const elementMap: Record<string, string> = {
+  cups: 'water',
+  pentacles: 'earth',
+  swords: 'air',
+  wands: 'fire',
+};
+
+defineProps<{
+  card: TarotCard;
+}>();
+
+const isFlipped = ref(true);
+
+const toggleFlip = () => (isFlipped.value = !isFlipped.value);
+</script>
+
+<template>
+  <div
+    class="card overflow-hidden p-2"
+    @click="toggleFlip"
+  >
+    <div
+      class="card__inner rounded-lg border-4 border-zinc-200"
+      :class="{ 'is-flipped': isFlipped }"
+    >
+      <div
+        class="card__front flex items-center text-center justify-center"
+        :class="{
+          [`card--${card.suit}`]: card.suit,
+          'card--major': card.arcana === 'major',
+        }"
+      >
+        <div>
+          <h1 class="text-3xl font-bold">{{ card.name }}</h1>
+
+          <hr class="my-2 mx-4 opacity-10 border-white" />
+
+          <p class="text-xl font-medium capitalize">
+            {{ card.number || card.court }}
+          </p>
+        </div>
+        <!-- <img
+            :src="card.image"
+            alt="The Fool Tarot Card"
+            class="h-full w-full object-cover"
+          /> -->
+      </div>
+      <div class="card__back">
+        <img
+          src="/images/tarot-back.png"
+          alt="Tarot Card Back Design"
+          class="h-full w-full object-cover"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+.card {
+  perspective: 1000px;
+  width: 200px;
+  height: 300px;
+  user-select: none;
+}
+
+.card__inner {
+  width: 100%;
+  height: 100%;
+  transition: transform 0.5s var(--v-ease-in-out);
+  transform-style: preserve-3d;
+  cursor: pointer;
+}
+
+.card__front,
+.card__back {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  backface-visibility: hidden;
+}
+
+.card__back {
+  transform: rotateY(180deg);
+}
+
+.is-flipped {
+  transform: rotateY(180deg);
+}
+
+/* colors */
+.card--major {
+  background: radial-gradient(
+      ellipse farthest-corner at right bottom,
+      #fedb37 0%,
+      #fdb931 8%,
+      #9f7928 30%,
+      #8a6e2f 40%,
+      transparent 80%
+    ),
+    radial-gradient(
+      ellipse farthest-corner at left top,
+      #ffffff 0%,
+      #ffffac 8%,
+      #d1b464 25%,
+      #5d4a1f 62.5%,
+      #5d4a1f 100%
+    );
+
+  color: black;
+}
+
+.card--cup {
+  background-color: theme('colors.blue.300');
+  color: theme('colors.blue.900');
+}
+
+.card--pentacle {
+  background-color: theme('colors.green.300');
+  color: theme('colors.green.900');
+}
+
+.card--sword {
+  background-color: theme('colors.yellow.300');
+  color: theme('colors.yellow.900');
+}
+
+.card--wand {
+  background-color: theme('colors.rose.300');
+  color: theme('colors.rose.900');
+}
+</style>
