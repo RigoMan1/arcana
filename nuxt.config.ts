d8abf5044cf81bdace163e38f2a1d74751369a4f
RigoMan1 <rodrigoalonso3781@gmail.com>
2024-03-13 06:15:41 -0500

    chore: setup eslint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: false },
-  modules: ['@nuxtjs/tailwindcss', '@vueuse/nuxt', '@pinia/nuxt'],
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@vueuse/nuxt',
+    '@pinia/nuxt',
+    '@nuxtjs/eslint-module',
+  ],
   tailwindcss: {
     exposeConfig: true,
     configPath: './tailwind.config.ts',
