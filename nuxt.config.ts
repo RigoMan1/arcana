df91d4e7d86030e592a604559166bc2e7d69c97b
RigoMan1 <rodrigoalonso3781@gmail.com>
2024-03-12 21:47:45 -0500

    add @pinia/nuxt module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: false },
-  modules: ['@nuxtjs/tailwindcss', '@vueuse/nuxt'],
+  modules: ['@nuxtjs/tailwindcss', '@vueuse/nuxt', '@pinia/nuxt'],
   tailwindcss: {
     exposeConfig: true,
     configPath: './tailwind.config.ts',
