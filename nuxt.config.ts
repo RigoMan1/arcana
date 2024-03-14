656f36204e17b22fdc62132f3b4e6e3cb3ac2d81
RigoMan1 <rodrigoalonso3781@gmail.com>
2024-03-13 22:19:21 -0500

    setup @nuxt/content and @tailwindcss/typography

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,10 +6,16 @@ export default defineNuxtConfig({
     '@vueuse/nuxt',
     '@pinia/nuxt',
     '@nuxtjs/eslint-module',
+    '@nuxt/content',
   ],
   tailwindcss: {
     exposeConfig: true,
     configPath: './tailwind.config.ts',
+  },
+  runtimeConfig: {
+    chatgpt: {
+      apiKey: process.env.OPENAI_API_KEY,
+    },
   },
   css: ['./assets/main.css'],
 });
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,12 +2,12 @@
 import type { ModuleOptions } from '@nuxtjs/tailwindcss';
 
 export default {
-	content: [],
-	theme: {
-		container: {
-			center: true,
-		},
-		extend: {},
-	},
-	plugins: [],
+  content: [],
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {},
+  },
+  plugins: [require('@tailwindcss/typography')],
 } satisfies ModuleOptions['config'];
