493f186e590496e23718802652aed309fd6a0e23
RigoMan1 <rodrigoalonso3781@gmail.com>
2024-07-22 02:35:58 -0500

    refactor: clean up and simplify logic for chatgpt models

--- server/api/chat-completion.ts
+++ server/api/chat-completion.ts
@@ -1,19 +1,13 @@
 import OpenAI from 'openai';
 import { createError, defineEventHandler, readBody } from 'h3';
 import { defaultOptions } from '../../constants/options';
-import {
-  MODEL_GPT_TURBO_3_5_0125,
-  MODEL_GPT_4_OMNI,
-  type IModel,
-} from '../../constants/models';
-import { modelMap } from '../../utils/model-map';
+import { MODEL_GPT_4_OMNI_MINI } from '../../constants/models';
 import { useRuntimeConfig } from '#imports';
 import { logitBias } from '../../constants/blacklist';
 
 export default defineEventHandler(async (event) => {
   const { messages, model, options } = await readBody(event);
 
-  // throw an error if the apiKey is not set
   if (!useRuntimeConfig().chatgpt.apiKey) {
     throw createError({
       statusCode: 403,
@@ -21,20 +15,16 @@ export default defineEventHandler(async (event) => {
     });
   }
 
-  // set-up configuration object and apiKEY
   const openai = new OpenAI({
     apiKey: useRuntimeConfig().chatgpt.apiKey,
   });
 
   try {
     const chatCompletion = await openai.chat.completions.create({
+      model: model || MODEL_GPT_4_OMNI_MINI,
       messages,
-      // model: !model ? modelMap[MODEL_GPT_4_OMNI] : modelMap[model as IModel],
-      // model: modelMap[MODEL_GPT_TURBO_3_5_0125],
-      model: 'gpt-4o-mini',
-      ...(options || defaultOptions),
-      function_call: 'auto',
       logit_bias: logitBias,
+      ...(options || defaultOptions),
     });
 
     const message = chatCompletion.choices[0].message;
@@ -42,7 +32,6 @@ export default defineEventHandler(async (event) => {
     return {
       role: message.role,
       content: message.content as string,
-      function_call: message.function_call,
     };
   } catch (error) {
     throw createError({
